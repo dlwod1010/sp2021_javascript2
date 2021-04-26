3dd139874010fa5ecc01cacccba4d7fd0c881c40
Jiae Lee <jiaelee1010@gmail.com>
2021-04-25 19:57:16 -0700

    convert nodejs app to express

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
-import http from "http";
-import querystring from "querystring";
-import { getAll, getItem } from "./data.js";
+// import querystring from "querystring";
+// import { getAll, getItem } from "./data.js";
 
-// import fs from "fs";
+'use strict'
+const express = require("express");
+// const bodyParser = require("body-parser")
+const app = express();
 
-http.createServer((req, res) => {
-    const url = req.url.toLowerCase();
-    const path = url.split("?")[0];
-    const query = url.split("?")[1];
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+// app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
-    switch(path) {
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(JSON.stringify(getAll()));
-            break;    
+// send static file as response
+app.get('/', (req, res) => {
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html'); 
+});
+
+// send plain text response
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('About page');
+});
+// define 404 handler
+app.use((req,res) => {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started'); 
+});
+
+
+
+
+// http.createServer((req, res) => {
+//     const url = req.url.toLowerCase();
+//     const path = url.split("?")[0];
+//     const query = url.split("?")[1];
+
+//     switch(path) {
+//         case '/':
+//             res.writeHead(200, {'Content-Type': 'text/plain'});
+//             res.end(JSON.stringify(getAll()));
+//             break;    
         
-        case '/about':        
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About me: Hi, my name is Jiae, I\'m currently attending a Web development program in Seattle Central College.');        
-            break; 
+//         case '/about':        
+//             res.writeHead(200, {'Content-Type': 'text/plain'});
+//             res.end('About me: Hi, my name is Jiae, I\'m currently attending a Web development program in Seattle Central College.');        
+//             break; 
 
-        case '/detail':        
-            let queryObject = querystring.parse(query);
-            const item = getItem(queryObject.name);
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(JSON.stringify(item));   
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Sorry, page not found');
-            break;
-    }
-}).listen(process.env.PORT || 3000);
+//         case '/detail':      
+//             let queryObject = querystring.parse(query);
+//             const item = getItem(queryObject.name);
+//             res.writeHead(200, {'Content-Type': 'text/plain'});
+//             res.end(JSON.stringify(item));   
+//             break;
+//         default:
+//             res.writeHead(404, {'Content-Type': 'text/plain'});
+//             res.end('Sorry, page not found');
+//             break;
+//     }
+// }).listen(process.env.PORT || 3000);
