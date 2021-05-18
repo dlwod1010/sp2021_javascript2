4201eb22642db5d04cd6bdcf40a94fafa5fd1cf2
Jiae Lee <jiaelee1010@gmail.com>
2021-05-17 16:43:35 -1000

    Creat API routes

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 import express from 'express';
 import exphbs from "express-handlebars"
 import { Machine } from "./models/machine.js";
+import cors from 'cors';
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.static('./public')); // set location for static files
 app.engine("handlebars", exphbs({defaultLayout: false}));
 app.set("view engine", "handlebars");
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('./public')); // set location for static files
+app.use('/api', cors());
+app.use(express.json());//Use to parse JSON bodies
 
 app.get('/', (req, res) => {
     Machine.find({}).lean().then((coffeeMachineList) => {
@@ -27,6 +30,44 @@ app.get('/about', (req, res) => {
     res.type('text/plain');
     res.send('About me: Hi, my name is Jiae, I\'m currently attending a Web development program in Seattle Central College.');
 }); 
+
+//api routes
+app.get('/api/machinelist', (req, res) => {
+    Machine.find({}).lean().then((coffeeMachineList) => {
+        res.json(coffeeMachineList);
+    })
+});
+
+app.get('/api/machinelist/itemdetail/:name', (req, res, next) => {
+    Machine.findOne({ name: req.params.name }).lean().then((machineItem) => {
+        res.json(machineItem);
+    })
+    .catch(err => next(err));
+});
+     
+app.post('/api/machinelist/additem', (req, res, next) => {
+    const machineToAdd = req.body;
+    Machine.create(machineToAdd).then((addedItem) => {
+        res.json(addedItem);      
+    })
+    .catch(err => next(err));
+});
+
+app.delete('/api/machinelist/:name', (req, res, next) => {
+    Machine.deleteOne({ name: req.params.name }).then((deletedItem) => {
+        res.json({
+            ok: deletedItem.ok === 1,
+            deletedCount: deletedItem.deletedCount
+        });
+    })
+    .catch(err => next(err));
+});
+
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('About me: Hi, my name is Jiae, I\'m currently attending a Web development program in Seattle Central College.');
+}); 
+
 
 // define 404 handler
 app.use((req,res) => { 
