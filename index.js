72e5e7dae0bf04d13a2ad65a9c3ec9e5452ca365
Jiae Lee <jiaelee1010@gmail.com>
2021-05-30 15:33:43 -0700

    able to read json data in html view

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict'
 import express from 'express';
-import exphbs from "express-handlebars"
+// import exphbs from "express-handlebars"
+import ejs from 'ejs';
 import { Machine } from "./models/machine.js";
 import cors from 'cors';
 
 const app = express();
 
-app.engine("handlebars", exphbs({defaultLayout: false}));
-app.set("view engine", "handlebars");
+//app.engine("handlebars", exphbs({defaultLayout: false}));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('./public')); // set location for static files
 app.use('/api', cors());
@@ -15,7 +17,7 @@ app.use(express.json());//Use to parse JSON bodies
 
 app.get('/', (req, res) => {
     Machine.find({}).lean().then((coffeeMachineList) => {
-        res.render('home', { data: coffeeMachineList })
+        res.render('home.html', { data: JSON.stringify(coffeeMachineList) })
     });    
 });
 
